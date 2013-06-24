27bed0a2bd9eded5f456ac9a3fb09437fad1e4b2
Erich Cordoba <erich.cm@gmail.com>
2013-06-24 14:06:32 -0500

    Protocolo implementado

--- Sensores_Raven.ino
+++ Sensores_Raven.ino
@@ -16,6 +16,8 @@
 #define MOSI       11
 #define SCK        13
  
+ 
+// Comandos internos del acelerometro
 #define LEER_INT_STATUS 0x58
 #define LEER_CTRL       0x04
 #define LEER_X_MSB      0x15
@@ -26,10 +28,28 @@
 #define LEER_Z_LSB      0x20
 #define ESCRIBIR_CTRL   0x07
 
+#define MAX_MILLIS_TO_WAIT 1000
+
+// Sensores habilitados 
+// Puede que no todos los sensores esten presentes en la tarjeta 
+// y por lo tanto se omite su lectura
+#define SENSOR_IRROMETER    1
+#define SENSOR_DISTANCIA    1
+#define SENSOR_PLUVIOMETRO  0
+#define SENSOR_ACELEROMETRO 1
+
+
 float Dx;
 float Dy;
 int pluv_tips;
+byte tipo_nodo;
 volatile int f_wdt = 1;
+unsigned long tiempo_inicial;
+
+union float_bytes{
+  byte b[4];
+  float val;
+}u_fb;
 
 // Configuraciones iniciales
 void setup() {            
@@ -50,29 +70,23 @@ void setup() {
   SPI.setDataMode(SPI_MODE0);
   digitalWrite(SCA_SELECT, HIGH);
   SPI.begin();
-  
-  //Configuracion WDT
-//  wdt_disable();
-  
-//  wdt_enable();
-  MCUSR = MCUSR & B11110111;
-  WDTCSR = WDTCSR | B00011000;
-  WDTCSR = B00100001;
-  WDTCSR = WDTCSR | B01000000;
-  MCUSR = MCUSR & B11110111;
-  
-  //WDTCSR |= (1 << WDCE) | (1 << WDE);
-  //WDTCSR |= (1 << WDP2) | (1 << WDP1) | (1 << WDP0);
-  //WDTCSR |= _BV(WDIE);
-  
-  
+ 
   // Configuracion Serial 
   Serial.begin(9600);
   
+  // Definiendo el tipo de nodo
+  tipo_nodo = 0;
+  tipo_nodo |= (SENSOR_ACELEROMETRO << 3) | (SENSOR_DISTANCIA << 2) | (SENSOR_IRROMETER << 1) | (SENSOR_PLUVIOMETRO);
   // Configuracion de interrupciones
   attachInterrupt(0, incrementaPluviometro, RISING); //Pluviometro
   pluv_tips = 0;
 }
+
+
+/**
+* Interrupcion 0 que cuenta los golpes del pluviometro
+* y almacena la informacion en una variable estatica
+*/
 
 void incrementaPluviometro(){
   static unsigned long t_ultima_int = 0;
@@ -80,29 +94,55 @@ void incrementaPluviometro(){
   if (t_interrupcion - t_ultima_int > 200)
   {
     pluv_tips++;
-    
-    Serial.println(pluv_tips);
   }
   t_ultima_int = t_interrupcion;
 
 }
 
+
+/**
+* 
+*
+*/
 ISR(WDT_vect){
   f_wdt++;
 }
 
+/**
+* Funcion que duerme la tarjeta apagando modulos selectivamente 
+*
+*/
+
 void dormirTarjeta(void){
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-  sleep_enable();
   
+  // Configura modo de ahorro de energia. 
+  set_sleep_mode(SLEEP_MODE_IDLE);
+  sleep_enable();
+  // Se desactivan selectivamente los modulos que no son necesarios
+  // dejando solamente al uart. 
+  
+  power_adc_disable();
+  power_spi_disable();
+  power_timer0_disable();
+  power_timer1_disable();
+  power_timer2_disable();
+  power_twi_disable();
+  
+  // A dormir
   sleep_mode();
   
-  // Regresa 
+  // Despertando...
   
   sleep_disable();
+  // Se activan todos los modulos 
   power_all_enable();
 }
 
+
+/**
+* Conjunto de funciones que activan o desactivan a los demas sensores 
+* en la tarjeta. 
+*/
 
 void encenderAcelerometro(){
   digitalWrite(ACT_ACC, HIGH);
@@ -131,8 +171,10 @@ void apagarIrrometer(){
   digitalWrite(ACT_IRR, LOW);
 }
 
-// Leer un registro del acelerometro. 
-// Necesita la direccion del registro como parametro
+/**
+* Lee un registro del acelerometro 
+* Recibe la direccion del registro y devuelve en un byte el contenido del registro
+*/
 
 byte leerRegAcelerometro(byte dirRegistro)
 {
@@ -144,6 +186,12 @@ byte leerRegAcelerometro(byte dirRegistro)
   return res;
 }
 
+/**
+* Escribe datos en un registro del acelerometo. 
+* Recibe la direccion y los datos del registro como parametro
+* Devuelve la respueta al comando enviado desde el acelerometro. 
+*/
+
 byte escribirRegAcelerometro(byte dirRegistro, byte datos){
   digitalWrite(SCA_SELECT, LOW);
   SPI.transfer(dirRegistro);
@@ -151,7 +199,10 @@ byte escribirRegAcelerometro(byte dirRegistro, byte datos){
   digitalWrite(SCA_SELECT, HIGH);
 }
 
-
+/**
+* Realiza el proceso de inicializacion del acelerometro. 
+* Verificando el estado de unos registro 
+*/
 byte iniciarAcelerometro(){
   byte result;
   encenderAcelerometro();
@@ -171,12 +222,23 @@ byte iniciarAcelerometro(){
   return 0;
 }
 
+
+/**
+* Lee el contenido del registro X del acelerometro 
+* Devuelve el valor de X en mg
+*/
+
 float leerEjeX(){
   int X;
   X = leerRegAcelerometro(LEER_X_MSB) << 8;
   X |= leerRegAcelerometro(LEER_X_LSB) & 0x00FF;
   return aMG(X);
 }
+
+/**
+* Lee el contenido del registro Y del acelerometro 
+* Devuelve el valor de Y en mg
+*/
 
 float leerEjeY(){
   int Y;
@@ -185,6 +247,10 @@ float leerEjeY(){
   return aMG(Y);
 }
 
+/**
+* Devuelve el promedio de 10 lecturas sobre el eje X 
+* del acelerometro
+*/
 
 float leerXPromedio(){
   float X;
@@ -193,6 +259,11 @@ float leerXPromedio(){
   }
   return X / 10;
 }
+
+/**
+* Devuelve el promedio de 10 lecturas sobre el eje Y 
+* del acelerometro
+*/
 
 float leerYPromedio(){
   float Y;
@@ -214,21 +285,179 @@ float aMG(int lectura)
   return (10 * (tmp_lectura) / 18);
 }
 
+/**
+* Lee entrada PWM desde el sensor ultrasonico
+* devuelve la distancia en centimetros
+*/
+
+float leerDist(){
+  float cm;
+  cm = ((float) pulseIn(IN_DIST, HIGH)) / 58; // Resolucion 58uS por cm. 
+  return cm;
+}
+
+/**
+* Funcion que devuelve el promedio de 10 lecturas del pulso del 
+* sensor ultrasonico.
+*/
+
+float promedioDist(){
+  float X;
+  for (int i=0; i<10; i++){
+    X += leerDist();
+  }
+  return X / 10;
+}
+
+
+/**
+* Devuelve periodo desde el generador de frecuencia
+* del tensiometro. 
+*/
+long leerIrrometer(){
+  long R;
+  R = pulseIn(IN_IRR, HIGH);
+  return R;
+}
+
+/**
+* Devuelve el promedio de periodos desde el generador
+* de frecuencia del tensiometro
+*/
+float promedioIrrometer(){
+  float R;
+  for (int i=0; i<10; i++){
+    R += (float) leerIrrometer();
+  }
+  return R / 10;
+}
+
+/*
+* Funcion que se encarga de procesar los comandos recibidos por la uart
+* tambien ejecuta el ciclo de recoleccion de datos de los sensores 
+* y por ultimo envia la informacion por la intefaz serial. 
+*/
+void procesadorComandos(){
+  // Esperamos por lo menos 6 bytes, si pasa un segundo sin datos disponibles, entonces salimos 
+  // a volver a dormir a la tarjeta. 
+  byte trama[6];
+  tiempo_inicial = millis();  
+  while ( (Serial.available() < 6) && ((millis() - tiempo_inicial) < MAX_MILLIS_TO_WAIT));
+  
+  if (Serial.available() >= 6)
+  {
+    for (int i=0; i<6; i++)
+      trama[i] = Serial.read(); // Leyendo bytes recibidos
+    if (trama[0] == 0x55 && trama[5] == 0xaa){ // Verificando las cabeceras de la trama
+      // Procesando comando 
+      switch(trama[4]){
+        case 1:
+          enviarDatosSensores();
+      }
+    }
+
+  } 
+}
+
+
+/**
+* Se encarga de recolectar los datos de los sensores y enviar la informacion
+* empaquetada en la estructura del protocolo. 
+* Se recolectan los datos de todos los sensores. 
+*/
+
+void enviarDatosSensores(){
+  int l_datos = SENSOR_ACELEROMETRO*8 + SENSOR_DISTANCIA*4 + SENSOR_IRROMETER*4 + SENSOR_PLUVIOMETRO;
+  
+  byte tramaDatos[l_datos + 7];
+  float tmp_x;
+  float tmp_y;
+  float tmp_dist;
+  float tmp_irr;
+  
+  // Agregando el tipo de nodo
+  tramaDatos[5] = tipo_nodo;
+  
+  // Obteniendo datos de los sensores 
+  if (SENSOR_ACELEROMETRO){
+    iniciarAcelerometro();
+    tmp_x = leerXPromedio();
+    tmp_y = leerYPromedio();
+    apagarAcelerometro();
+    
+    // Almacenando variables en la trama 
+    u_fb.val = tmp_x;
+    for (int i=0; i<4; i++)
+      tramaDatos[6 + i] = u_fb.b[i];
+    u_fb.val = tmp_y;
+    for (int i=0; i<4; i++)
+      tramaDatos[10 + i] = u_fb.b[i];
+  }
+  if (SENSOR_DISTANCIA){
+    encenderMaxSonar();
+    tmp_dist = promedioDist();
+    apagarMaxSonar();
+    
+    u_fb.val = tmp_dist;
+    for (int i=0; i<4; i++)
+      tramaDatos[14 + i] = u_fb.b[i];
+  }
+  if (SENSOR_IRROMETER){
+    encenderIrrometer();
+    tmp_irr = promedioIrrometer();
+    apagarIrrometer();
+    
+    u_fb.val = tmp_irr;
+    for (int i=0; i<4; i++)
+      tramaDatos[14 + SENSOR_DISTANCIA*4 + i] = u_fb.b[i];
+  }
+  
+  // Armando cabeceras 
+  tramaDatos[0] = 0x55;
+  tramaDatos[l_datos + 6] = 0xAA;
+  // Logitud de trama 
+  tramaDatos[1] = 0;
+  tramaDatos[2] = (byte) (l_datos + 7) & 0x00FF; // 22 bytes
+  
+  // Identificador y comando 
+  tramaDatos[3] = 0;
+  tramaDatos[4] = 1; // Comando de respuesta
+ 
+  // Enviando la trama por el puerto serial 
+ 
+  for (int i=0; i<(l_datos + 7); i++)
+    Serial.write(tramaDatos[i]);
+}
+
+/**
+*
+*
+*/
+
+
+
 
 // Rutina principal
 void loop() {
-//  while(!(Serial.available()));
-//  Serial.read();
-  Serial.println("Durmiendo");
+  long antes, despues;
+  float d;
+  Serial.println("Durmiendo tarjeta..");
   delay(100);
   dormirTarjeta();
-  digitalWrite(ACT_ACC, HIGH);
-  delay(1000);
-  digitalWrite(ACT_DIST, HIGH);
-  delay(1000);
-  digitalWrite(ACT_IRR, HIGH);
-  delay(1000);
+  if (Serial.available()){
+    procesadorComandos();
+  }
   Serial.println("Desperto");
+//  while(!(Serial.available()));
+//  Serial.read();
+//  Serial.println("Durmiendo");
+//  delay(100);
+//  dormirTarjeta();
+//  digitalWrite(ACT_ACC, HIGH);
+//  delay(1000);
+
+  delay(1000);
+
 
 
 /*
